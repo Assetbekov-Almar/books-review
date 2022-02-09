a86b262aebc0ead46e37584ace3cd394a63d620b
almar-a <almar-a@auto.kz>
2022-02-09 20:14:51 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,31 @@
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
 
-self.addEventListener('install', function(event) {
-	console.log('[ServiceWorker] Install');
 
-	event.waitUntil((async () => {
-		const cache = await caches.open(CACHE_NAME);
-		// Setting {cache: 'reload'} in the new request will ensure that the response
-		// isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-		await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-	})());
+'use strict';
 
-	self.skipWaiting();
-});
+const files = [
+	'/',
+	'/console.css',
+	'/console.js',
+	'/favicon.ico',
+	'/favicon.png',
+	'/manifest.json',
+];
 
-self.addEventListener('activate', (event) => {
-	console.log('[ServiceWorker] Activate');
-	event.waitUntil((async () => {
-		// Enable navigation preload if it's supported.
-		// See https://developers.google.com/web/updates/2017/02/navigation-preload
-		if ('navigationPreload' in self.registration) {
-			await self.registration.navigationPreload.enable();
-		}
-	})());
+self.addEventListener('install', (event) => event.waitUntil(
+	caches.open('v1').then((cache) => cache.addAll(files))
+));
 
-	// Tell the active service worker to take control of the page immediately.
-	self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-	// console.log('[Service Worker] Fetch', event.request.url);
-	if (event.request.mode === 'navigate') {
-		event.respondWith((async () => {
-			try {
-				const preloadResponse = await event.preloadResponse;
-				if (preloadResponse) {
-					return preloadResponse;
-				}
-
-				const networkResponse = await fetch(event.request);
-				return networkResponse;
-			} catch (error) {
-				console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-				const cache = await caches.open(CACHE_NAME);
-				const cachedResponse = await cache.match(OFFLINE_URL);
-				return cachedResponse;
-			}
-		})());
-	}
+self.addEventListener('fetch', (event) => {
+	event.respondWith(caches.match(event.request).then((response) => {
+		if (response !== undefined) return response;
+		return fetch(event.request).then((response) => {
+			const responseClone = response.clone();
+			caches.open('v1').then((cache) => {
+				cache.put(event.request, responseClone);
+			});
+			return response;
+		}).catch((error) => {
+			throw error;
+		});
+	}));
 })
