d2af0cd7205202aec6b60125118a431f89078733
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-01 22:07:43 +0600

    Updates

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ Notification.requestPermission(function(status) {
 })
 
 function displayNotification() {
-  console.log('asd')
   if (Notification.permission === 'granted') {
-    console.log('asd')
     navigator.serviceWorker.getRegistration().then(function(reg) {
-      reg.showNotification('Hello world!')
+      const options = {
+        body: 'Here is a notification body!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+      };
+      reg.showNotification('Hello world!', options);
     });
   }
 }
