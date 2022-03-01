0e03eda4841b3a7fb01fb2f5eda59fe16a4bce77
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-01 22:21:20 +0600

    Updates

--- main.js
+++ main.js
@@ -15,14 +15,15 @@ function displayNotification() {
     //   };
     //   reg.showNotification('Hello world!', options);
     // });
-    new Notification('Hello!',  {
-          body: 'Here is a notification body!',
-          vibrate: [100, 50, 100],
-          data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-          },
-        })
+    new Notification('Hello!', {
+      body: 'Here is a notification body!',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      },
+    })
+  }
 }
 
 document.querySelector('.notificationButton').addEventListener('click', displayNotification)
