92c801f2676111b84894a1c2cf4a9d5634769abf
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-01 22:48:40 +0600

    Updates

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
+let swRegistration = null
+
+initializeApp();
+
+function initializeApp() {
+  if ("serviceWorker" in navigator && "PushManager" in window) {
+    console.log("Service Worker and Push is supported");
+
+    //Register the service worker
+    navigator.serviceWorker
+      .register("../../sw.js")
+      .then(swReg => {
+        console.log("Service Worker is registered", swReg);
+
+        swRegistration = swReg;
+      })
+      .catch(error => {
+        console.error("Service Worker Error", error);
+      });
+  } else {
+    console.warn("Push messaging is not supported");
+  }
+}
 Notification.requestPermission(function(status) {
   console.log('Notification permission status:', status)
 })
 
 function displayNotification() {
   if (Notification.permission === 'granted') {
-    navigator.serviceWorker.getRegistration().then(function(reg) {
-      console.log(reg)
       const options = {
         body: 'Here is a notification body!',
         vibrate: [100, 50, 100],
@@ -14,9 +35,10 @@ function displayNotification() {
           primaryKey: 1
         },
       };
-      reg.showNotification('Hello world!', options);
-    });
+      swRegistration.showNotification('Hello world!', options)
   }
 }
 
 document.querySelector('.notificationButton').addEventListener('click', displayNotification)
+
+
