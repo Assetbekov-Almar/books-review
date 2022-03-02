cb590cdbe39e78d0f0166f5bd49ac626dea568ea
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-02 23:54:57 +0600

    Updates

--- main.js
+++ main.js
@@ -27,13 +27,14 @@ function displayNotification() {
   if (Notification.permission === 'granted') {
     // navigator.serviceWorker.getRegistration().then(function(reg) {
       var options = {
-        body: 'Here is a notification body!',
+        body: 'A new book has been added!',
         icon: './notification_logo.png',
         vibrate: [100, 50, 100],
         data: {
           dateOfArrival: Date.now(),
           primaryKey: 1
         },
+
         // actions: [
         //   {action: 'explore', title: 'Explore this new world',
         //     icon: './contact.png'},
@@ -41,7 +42,7 @@ function displayNotification() {
         //     icon: './library.png'},
         // ]
       };
-      new Notification('Hello world!', options);
+      new Notification('Library update!', options);
     // });
   }
 }
