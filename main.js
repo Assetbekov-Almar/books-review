49661852930dab328e30efd1555ea62acb90921c
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-08 16:20:53 +0600

    Updates

--- main.js
+++ main.js
@@ -38,6 +38,12 @@ function initializeApp() {
           } else {
             // We have a subscription, update the database
             console.log('Subscription object: ', sub);
+            var endpoint = sub.endpoint;
+            var auth = sub.keys.auth;
+            var p256dh = sub.keys.p256dh;
+            console.log(endpoint)
+            console.log(auth)
+            console.log(p256dh)
           }
         });
 
