e374b8b99663d6f7f8ed91f9abb558b7c0eb057a
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-08 13:41:57 +0600

    Updates

--- main.js
+++ main.js
@@ -58,22 +58,20 @@ function displayNotification() {
 }
 
 function subscribeUser() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(function(reg) {
-      console.log(reg)
-      // reg.pushManager.subscribe({
-      //   userVisibleOnly: true
-      // }).then(function(sub) {
-      //   console.log('Endpoint URL: ', sub.endpoint);
-      // }).catch(function(e) {
-      //   if (Notification.permission === 'denied') {
-      //     console.warn('Permission for notifications was denied');
-      //   } else {
-      //     console.error('Unable to subscribe to push', e);
-      //   }
-      // });
-    })
-  }
+  navigator.serviceWorker.ready.then(function (reg) {
+    console.log(reg)
+    // reg.pushManager.subscribe({
+    //   userVisibleOnly: true
+    // }).then(function(sub) {
+    //   console.log('Endpoint URL: ', sub.endpoint);
+    // }).catch(function(e) {
+    //   if (Notification.permission === 'denied') {
+    //     console.warn('Permission for notifications was denied');
+    //   } else {
+    //     console.error('Unable to subscribe to push', e);
+    //   }
+    // });
+  })
 }
 
 document.querySelector('.notificationButton').addEventListener('click', () => {
