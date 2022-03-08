64f5a165023610903cfbace44de5d57196573f32
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-08 13:15:48 +0600

    Updates

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ document.querySelector('.notificationButton').addEventListener('click', () => {
 
 function subscribeUser() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistration().then(function(reg) {
+    navigator.serviceWorker.getRegistration('https://assetbekov-almar.github.io/books-review/').then(function(reg) {
       console.log(reg)
       reg.pushManager.subscribe({
         userVisibleOnly: true
