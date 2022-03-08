e63d6e93fbc3b1bfab81061dd3fb81e60caa2828
assetbekov-almar <almar.asetbekov@gmail.com>
2022-03-08 12:41:49 +0600

    Updates

--- main.js
+++ main.js
@@ -11,13 +11,17 @@ function initializeApp() {
         console.log("Service Worker is registered", swReg);
 
         swReg.pushManager.getSubscription().then(function(sub) {
-          if (sub === null) {
-            // Update UI to ask user to register for Push
-            console.log('Not subscribed to push service!');
-          } else {
-            // We have a subscription, update the database
-            console.log('Subscription object: ', sub);
-          }
+          swReg.pushManager.subscribe({
+            userVisibleOnly: true
+          }).then(function(sub) {
+            console.log('Endpoint URL: ', sub.endpoint);
+          }).catch(function(e) {
+            if (Notification.permission === 'denied') {
+              console.warn('Permission for notifications was denied');
+            } else {
+              console.error('Unable to subscribe to push', e);
+            }
+          });
         });
 
       })
@@ -64,20 +68,20 @@ document.querySelector('.notificationButton').addEventListener('click', () => {
 
 function subscribeUser() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(function(reg) {
-      console.log(reg)
-      reg.pushManager.subscribe({
-        userVisibleOnly: true
-      }).then(function(sub) {
-        console.log('Endpoint URL: ', sub.endpoint);
-      }).catch(function(e) {
-        if (Notification.permission === 'denied') {
-          console.warn('Permission for notifications was denied');
-        } else {
-          console.error('Unable to subscribe to push', e);
-        }
-      });
-    })
+    // navigator.serviceWorker.ready.then(function(reg) {
+    //   console.log(reg)
+    //   reg.pushManager.subscribe({
+    //     userVisibleOnly: true
+    //   }).then(function(sub) {
+    //     console.log('Endpoint URL: ', sub.endpoint);
+    //   }).catch(function(e) {
+    //     if (Notification.permission === 'denied') {
+    //       console.warn('Permission for notifications was denied');
+    //     } else {
+    //       console.error('Unable to subscribe to push', e);
+    //     }
+    //   });
+    // })
   }
 }
 
